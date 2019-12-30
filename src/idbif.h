1995849588d8b6530303263cda4a85455f522368
Michael Schwager <mschw@fb.com>
2019-12-30 09:57:21 -0800

    Moved stuff into proj and src directories, haven't tried compiling yet; deleted unused icon sets

---
+++ src/idbif.h
@@ -0,0 +1,16 @@
+#ifndef IDBIF_H
+#define IDBIF_H
+
+// This is the abstract interface to the interface between the core and the Qt database
+
+
+#include <iostream>
+
+class IDbIf
+{
+public:
+    virtual ~IDbIf() {}
+    virtual void createDatabase(std::string) = 0;
+};
+
+#endif // DBIF_H
